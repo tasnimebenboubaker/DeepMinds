e9307c79203661e5369ba87bacb6b984df3bfaa5
tasnimebbk <benboubakertasnime@gmail.com>
2026-01-24 22:58:01 +0100

    added data gen notebook

---
+++ backend/init_qdrant.py
@@ -0,0 +1,22 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance
+from backend.config import QDRANT_URL, QDRANT_API_KEY
+
+# connexion à Qdrant Cloud
+client = QdrantClient(
+    url=QDRANT_URL,
+    api_key=QDRANT_API_KEY
+)
+
+COLLECTION_NAME = "DeepMinds"
+
+# Crée ou recrée la collection (supprime si existante)
+client.recreate_collection(
+    collection_name=COLLECTION_NAME,
+    vectors_config=VectorParams(
+        size=384,       # taille de l'embedding MiniLM
+        distance=Distance.COSINE
+    )
+)
+
+print("✅ Collection créée")
